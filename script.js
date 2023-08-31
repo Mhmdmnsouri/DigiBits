aa35c60ce7acad7f7c42d5c93bfb4d69028b3c7d
Mhmdmnsouri <Bitshawcoder@gmail.com>
2023-08-31 12:13:03 +0330

    script code for menu

--- script.js
+++ script.js
@@ -10,9 +10,30 @@ let settings = {
     'headers': {}
 }
 
-$.ajax(settings).done(function (response){
+$.ajax(settings).done(function (response) {
     console.log(response)
     btcprice.innerHTML = response.bitcoin.usd;
     ethprice.innerHTML = response.ethereum.usd;
     dogeprice.innerHTML = response.dogecoin.usd;
-}) 
+})
+
+const navHeaderElem = document.querySelector('.navHeader');
+const burgerElem = document.querySelector('.burger');
+const barsElem = document.querySelector('.bars');
+const closeElem = document.querySelector('.close');
+const logoElem = document.querySelector('.logo');
+const menuIconElem = document.querySelector('.menuIcon');
+
+function toggleMenu() {
+    if (navHeaderElem.classList.contains("showMenu")) {
+        navHeaderElem.classList.remove("showMenu");
+        closeElem.style.display = "none";
+        barsElem.style.display = "block";
+    } else {
+        navHeaderElem.classList.add("showMenu");
+        closeElem.style.display = "block";
+        barsElem.style.display = "none";
+    }
+}
+
+burgerElem.addEventListener("click", toggleMenu);
